6c544734bd1175753d425895ab36261497084cce
Barış Kandemir <48553738+bariskndmr@users.noreply.github.com>
2021-12-27 09:39:42 +0300

    restaurant items created

--- src/Components/RestauranItem/RestaurantItem.js
+++ src/Components/RestauranItem/RestaurantItem.js
@@ -1,12 +1,82 @@
 import React from 'react';
 import {View, Image, TouchableOpacity, Text} from 'react-native';
 
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Styles from './RestaurantItem.style';
+
+const localRestaurants = [
+  {
+    name: 'Beachside Bar',
+    image_url:
+      'https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg',
+    categories: ['Cafe', 'Bar'],
+    price: '$$',
+    reviews: 1244,
+    rating: 4.5,
+  },
+  {
+    name: 'Benihana',
+    image_url:
+      'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
+    categories: ['Cafe', 'Bar'],
+    price: '$$',
+    reviews: 1244,
+    rating: 3.7,
+  },
+  {
+    name: "India's Grill",
+    image_url:
+      'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
+    categories: ['Indian', 'Bar'],
+    price: '$$',
+    reviews: 700,
+    rating: 4.9,
+  },
+];
+
 const RestaurantItem = () => {
   return (
-    <View>
-      <Text>Restauran</Text>
-    </View>
+    <TouchableOpacity activeOpacity={1}>
+      {localRestaurants.map((restaurant, index) => (
+        <View key={index} style={Styles.container}>
+          <RestaurantImage image={restaurant.image_url} />
+          <RestaurantInfo name={restaurant.name} rating={restaurant.rating} />
+        </View>
+      ))}
+    </TouchableOpacity>
   );
 };
 
 export default RestaurantItem;
+
+const RestaurantImage = props => {
+  return (
+    <>
+      <Image
+        style={Styles.image}
+        source={{
+          uri: props.image,
+        }}
+      />
+      <MaterialCommunityIcons
+        style={Styles.heartIcon}
+        name="heart-outline"
+        size={24}
+      />
+    </>
+  );
+};
+
+const RestaurantInfo = props => {
+  return (
+    <View style={Styles.ınnerContainer}>
+      <View>
+        <Text style={Styles.title}>{props.name}</Text>
+        <Text style={Styles.time}>30-45 Min. </Text>
+      </View>
+      <View style={Styles.pointContainer}>
+        <Text style={Styles.point}>{props.rating}</Text>
+      </View>
+    </View>
+  );
+};
--- src/Components/RestauranItem/RestaurantItem.style.js
+++ src/Components/RestauranItem/RestaurantItem.style.js
@@ -1,5 +1,39 @@
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-  container: {},
+  container: {
+    backgroundColor: '#fff',
+    padding: 10,
+    marginVertical: 10,
+  },
+  image: {
+    width: '100%',
+    height: 180,
+    marginBottom: 10,
+  },
+  heartIcon: {
+    position: 'absolute',
+    right: 20,
+    top: 20,
+    color: '#fff',
+  },
+  ınnerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  pointContainer: {
+    backgroundColor: '#eee',
+    padding: 5,
+    borderRadius: 50,
+  },
+  title: {
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  time: {
+    fontSize: 12,
+    fontWeight: '300',
+    marginTop: 3,
+  },
 });
